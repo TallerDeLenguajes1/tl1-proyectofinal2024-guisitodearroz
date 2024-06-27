594aacb7cd836e0f82b08d7f814f28eb555a5b6f
Cortez Diego <dc936572@gmail.com>
2024-06-27 20:28:21 -0300

    coreccion de la ApiServicio

--- ApiServicio.cs
+++ ApiServicio.cs
@@ -2,55 +2,29 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Text.Json;
+using clasesApi;
+using personajes;
 
 public class ApiService
 {
-    private static readonly string urlApi = "https://random-data-api.com/api/v2/users?size=1";
+    //utilizar 1 sola api la otra hacerla con un num
     private static readonly string urlProvincias = "https://apis.datos.gob.ar/georef/api/provincias";
-
-    public static async Task<(string Nombre, string Provincia, DateTime FechaNacimiento)> ObtenerDatosAsync()
+    private static readonly HttpClient client = new HttpClient();
+    await GetNombreApi();
+    Apodo apo= new Apodo();
+    private static async Task<Persona> GetNombreApi()
     {
-        using HttpClient client = new HttpClient();
-
+        private static readonly string urlApi = "https://random-data-api.com/api/v2/users?size=1";
         HttpResponseMessage response = await client.GetAsync(urlApi);
-        if (response.IsSuccessStatusCode)
-        {
-            string jsonResponse = await response.Content.ReadAsStringAsync();
-            JsonDocument doc = JsonDocument.Parse(jsonResponse);
-
-            JsonElement user = doc.RootElement;
-            string nombre = $"{user.GetProperty("first_name").GetString()} {user.GetProperty("last_name").GetString()}";
-            string provincia = await ObtenerProvinciaAleatoria();
-            DateTime fechaNacimiento = DateTime.Parse(user.GetProperty("date_of_birth").GetString());
-
-            return (nombre, provincia, fechaNacimiento);
-        }
-        else
-        {
-            throw new Exception("Error al obtener datos de la API.");
-        }
-    }
-
-    private static async Task<string> ObtenerProvinciaAleatoria()
-    {
-        using HttpClient client = new HttpClient();
-
-        HttpResponseMessage response = await client.GetAsync(urlProvincias);
-        if (response.IsSuccessStatusCode)
-        {
-            string jsonResponse = await response.Content.ReadAsStringAsync();
-            JsonDocument doc = JsonDocument.Parse(jsonResponse);
-
-            JsonElement root = doc.RootElement.GetProperty("provincias");
-            Random rnd = new Random();
-            int index = rnd.Next(root.GetArrayLength());
-            string nombreProvincia = root[index].GetProperty("nombre").GetString();
-
-            return nombreProvincia;
-        }
-        else
-        {
-            throw new Exception("Error al obtener las provincias de la API.");
-        }
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+        PersonaDatos persona = JsonSerializer.Deserialize<Persona>(responseBody);
+        persona.apodo= apo.obtenerApodo();
+        persona.provincia=;
+        return Persona;
     }
 }
+
+
+
+
---
+++ ClasesApi.cs
@@ -0,0 +1,62 @@
+namespace clasesApi;
+//clases para la api que me trae nombre, apellido y fecha de nacimiento
+ public class Address
+    {
+        public string city { get; set; }
+        public string street_name { get; set; }
+        public string street_address { get; set; }
+        public string zip_code { get; set; }
+        public string estado { get; set; }
+        public string pas { get; set; }
+        public Coordenadas coordenadas { get; set; }
+    }
+
+    public class Coordenadas
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class CreditCard
+    {
+        [JsonProperty("cc_number ")]
+        public string cc_number { get; set; }
+    }
+
+    public class Empleo
+    {
+        public string title { get; set; }
+        public string key_skill { get; set; }
+    }
+
+    public class PersonaDatos
+    {
+        public int id { get; set; }
+        public string uid { get; set; }
+        public string contrasea { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string apodo {get; set;}
+        public string provincia {get; set;}
+
+        [JsonProperty("nombre de usuario")]
+        public string nombredeusuario { get; set; }
+        public string email { get; set; }
+        public string avatar { get; set; }
+        public string gender { get; set; }
+        public string phone_number { get; set; }
+        public string social_insurance_number { get; set; }
+        public string date_of_birth { get; set; }
+        public Empleo empleo { get; set; }
+        public Address address { get; set; }
+        public CreditCard credit_card { get; set; }
+        public Suscripción suscripcin { get; set; }
+    }
+
+    public class Suscripción
+    {
+        public string plan { get; set; }
+        public string status { get; set; }
+        public string mtodo_pago { get; set; }
+        public string term { get; set; }
+    }
